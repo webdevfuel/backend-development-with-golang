e868851b4c994210cc707deed8043f5e7e91570d
Emanuel Turis <hello@emanuelturis.com>
2023-12-02 15:09:53 +0000

    chi-router-sub-router

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,8 +11,25 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Get("/foo", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+	r.Mount("/bar", exampleSubRouter())
+	http.ListenAndServe("localhost:3000", r)
+}
+
+func exampleSubRouter() http.Handler {
+	r := chi.NewRouter()
+	r.Use(ExampleMiddleware)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
-	http.ListenAndServe("localhost:3000", r)
+	return r
+}
+
+func ExampleMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello, from middleware!")
+		next.ServeHTTP(w, r)
+	})
 }
