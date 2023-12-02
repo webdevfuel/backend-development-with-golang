60c3c14ceaa628c9d5996ee446d40a3a79ccfa93
Emanuel Turis <hello@emanuelturis.com>
2023-12-02 15:04:25 +0000

    chi-router-example-route-middleware

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,8 +11,15 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.With(ExampleRouteMiddleware).Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
 	http.ListenAndServe("localhost:3000", r)
+}
+
+func ExampleRouteMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello, from middleware!")
+		next.ServeHTTP(w, r)
+	})
 }
