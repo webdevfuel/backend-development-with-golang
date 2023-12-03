19cef37b54e75e6f18956717eec9f402a62c319a
Emanuel Turis <hello@emanuelturis.com>
2023-12-03 14:06:05 +0000

    cookies

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,8 +11,28 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world!"))
+	r.Delete("/foo", func(w http.ResponseWriter, r *http.Request) {
+		cookie := http.Cookie{
+			Name:   "message",
+			Value:  "Hello, from cookies!",
+			MaxAge: -1,
+		}
+		http.SetCookie(w, &cookie)
+	})
+	r.Post("/foo", func(w http.ResponseWriter, r *http.Request) {
+		cookie := http.Cookie{
+			Name:  "message",
+			Value: "Hello, from cookies!",
+		}
+		http.SetCookie(w, &cookie)
+	})
+	r.Get("/bar", func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("message")
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		log.Println(cookie.Value)
 	})
 	http.ListenAndServe("localhost:3000", r)
 }
